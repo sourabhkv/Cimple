03715e0820542ecf292b523c356e9c562edbeea1
sourabhkv <55890376+sourabhkv@users.noreply.github.com>
2022-06-10 19:51:08 +0530

    Create specialchar.c

---
+++ specialchar.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main(){
+	//printing special char
+	//printing "\"
+	printf("\\","\n");
+	//printing " quotes
+	printf("I am from \"c\" section"); //or printf("I am from 'c' section");
+}
