adec796f02edf977a2fd7dc43268d8715a5fe94c
Sourabh kumar verma <55890376+sourabhkv@users.noreply.github.com>
2022-07-07 20:31:36 +0530

    Create starpattern1.c

---
+++ starpattern1.c
@@ -0,0 +1,23 @@
+/* Sample output
+ *
+ * *
+ * * *
+ * * * *
+ * * * * *
+ * * * * * *
+ * * * * * * *
+ * * * * * * * *
+ * * * * * * * * *
+*/
+#include <stdio.h>
+void main(){
+	int n;
+	printf("enter n:");
+	scanf("%d",&n);
+	for (int i=1;i<=n;i++){
+		for (int j=1;j<=i;j++){
+			printf(" *");
+		}
+		printf("\n");
+	}
+}
