b4e68034ada832e508894b7d7f02e6fa0354ac84
Sourabh kumar verma <55890376+sourabhkv@users.noreply.github.com>
2022-07-16 20:18:56 +0530

    Create star pattern 2.c

---
+++ star pattern 2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main(){
+	int n;
+	printf("enter n:");
+	scanf("%d",&n);
+	for (int i=1;i<=5;i++){
+		for (int j=n-1;j>=i;j--){
+			printf("   ");
+		}
+		for (int k=1;k<=n;k++){
+			printf("  *");
+		}
+		printf("\n");
+	}
+}
