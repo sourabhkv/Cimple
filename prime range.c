d2b749477b4e20c45ced96030dea63552f90e497
Sourabh kumar verma <55890376+sourabhkv@users.noreply.github.com>
2022-07-18 22:06:34 +0530

    Update prime range.c

--- prime range.c
+++ prime range.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
-void main(){
+void main(){//Speed is priority for this program
 	int count=1,n;
 	printf("enter upper limit :");
 	scanf("%d",&n);
-	for (int i=3;i<n;i+=2){//skipping even numbers except 2 i.e. included in count default value
-		int factor=0;
-		for (int j=1;j<i+1;j++){
-			if (i%j==0){
-				factor=factor+1;
-				if (factor>2){
-					break;
+	if (n==2){
+		printf("2 is prime");
+	}
+	else if (n!=2 && n>2){
+		for (int i=3;i<n;i+=2){//skipping even numbers except 2 i.e. included in count default value
+			int factor=0;
+			for (int j=1;j<i+1;j++){
+				if (i%j==0){
+					factor=factor+1;
+					if (factor>2){
+						break;
+					}
 				}
 			}
+			if (factor==2){
+				count+=1;
+			}
 		}
-		if (factor==2){
-			count+=1;
-		}
+		printf("%d",count);
 	}
-	printf("%d",count);
 }
