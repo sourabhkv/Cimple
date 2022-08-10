652d593bc3b984a522ad0fcbff075e15bf013b1b
Sourabh kumar verma <55890376+sourabhkv@users.noreply.github.com>
2022-08-10 20:37:15 +0530

    Create number pattern2.c

---
+++ pattern/number pattern2.c
@@ -0,0 +1,31 @@
+/*
+
+output
+enter no. of rows :5
+        1
+      1   2
+    1   2   3
+  1   2   3   4
+1   2   3   4   5
+
+
+*/
+#include <stdio.h>
+void main()
+{
+	int i,j,k,n,r;
+	printf("enter no. of rows :");
+	scanf("%d",&n);
+	for (i=1;i<=n;i++)
+	{
+		for (k=n-1;k>=i;k--)
+		{
+			printf("  ");
+		}
+		for (j=1;j<i+1;j++)
+		{
+			printf("%d   ",j);
+		}
+		printf("\n");
+	}
+}
