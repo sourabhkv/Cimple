0a598e4a3d4c544d2e434dc041c8d1acbef47f37
Sourabh kumar verma <55890376+sourabhkv@users.noreply.github.com>
2022-08-27 19:55:35 +0530

    Create fibonacci.c

---
+++ recursion/fibonacci.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int fib(int n){
+	if (n==0)
+		return 0;
+	else if (n==1)
+		return 1;
+	else
+		return fib(n-1)+fib(n-2);
+}
+void main(){
+	int n;
+	printf("enter n:");
+	scanf("%d",&n);
+	printf("fib(%d) = %d",n,fib(n));
+}
