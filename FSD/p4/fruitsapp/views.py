6d3ab1d887ce9f383426d7642c7bc49cbd336166
Sourabh kumar verma <55890376+sourabhkv@users.noreply.github.com>
2024-06-24 08:50:28 +0530

    adding p4 FSD

---
+++ FSD/p4/fruitsapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request): 
+    return render(request,'home.html') 
+
+def contactus(request): 
+     return render(request,'contactus.html') 
+def aboutus(request): 
+     return render(request,'about.html')
